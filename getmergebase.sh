d9afe35080e5ce6123b8224e9bdf6aef8cb6baaf
Gabriel Huber <mail@gabrielhuber.at>
2023-03-08 13:46:13 +0100

    Create branch instead of working on detached head in getmergebase.sh

--- getmergebase.sh
+++ getmergebase.sh
@@ -5,9 +5,10 @@ set -e
 targetRepository="https://github.com/$GITHUB_REPOSITORY"
 targetBranch="$GITHUB_BASE_REF"
 
+git branch merge-branch
 cp packages.json packages.json.bak
 git fetch "$targetRepository" "$targetBranch:base"
 # Get packages.json at the branching point
-git checkout "$(git merge-base HEAD base)" packages.json
+git checkout "$(git merge-base merge-branch base)" packages.json
 mv packages.json packages_old.json
 mv packages.json.bak packages.json
